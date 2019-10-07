2c11df068c5a94a1b921ef34e59ced81f646bb36
Juan Esteban Hernández <factoriatech@gmail.com>
2019-10-07 12:16:54 -0400

    added card on home

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import { StyleSheet, View,Dimensions, ActivityIndicator,ScrollView, Text } from 
 import { Block, theme } from 'galio-framework';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-
+import FCard from '../components/FCard';
 
 const GET_NEWS = gql`
 query{
@@ -18,6 +18,9 @@ query{
   }
 }`;
 
+const cont = "Mas abajo la app carga unas noticias. Ahora bien, esto la primera vez carga lento, y es aproposito, ya que el app hace un request vía GraphQL a un servidor hosteado en Heroku con Hasura. Además dicho servidor tiene remotamente conetado un blog hecho en wordpress, el cual es de donde estas noticas estan siendo conseguidas. Todo esto en producción se hace muy rapido, la razon por la que es lento es porque en Heroku esta instancia esta en un plan gratuito y es una arquitectura Serverless, la cual esta apagado mientras no se utilice.";
+
+
 import { Card } from '../components';
 import articles from '../constants/articles';
 const { width } = Dimensions.get('screen');
@@ -29,6 +32,11 @@ const Home = () => {
          <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.articles}>
+          <FCard 
+          title="Bienvenido a nuestra App de Demo!" 
+          content={cont}
+          imagen="https://res.cloudinary.com/factoriatech/image/upload/v1570464189/Microsoft-bosque-programming-language.jpg" 
+          />
           <Block flex>
             {loading ? <View style={[styles.container, styles.horizontal]}>
         <ActivityIndicator size="large" color="#0000ff" /></View> : data.posts.nodes.map(p => {
